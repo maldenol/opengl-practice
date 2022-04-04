8d9253773523f0aec8bed1bd8862e8787905868f
maldenol <mineden2300mo@gmail.com>
2022-04-04 21:03:06 +0300

    Make GLFW context switching properly
    Apply architecture, style and documentation fixes.

--- src/triangle/main.cpp
+++ src/triangle/main.cpp
@@ -1,8 +1,11 @@
 // STD
+#include <atomic>
 #include <chrono>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
+#include <vector>
 
 // Qt5
 #include <QDateTime>
@@ -22,7 +25,12 @@ using namespace std::chrono_literals;
 // Global constants
 constexpr unsigned int          kWidth                 = 800;
 constexpr unsigned int          kHeight                = 600;
+constexpr std::chrono::duration kRenderCycleInterval   = 16ms;
 constexpr std::chrono::duration kShaderWatcherInterval = 500ms;
+
+// Global variables
+std::atomic<bool> gShaderWatcherIsRunning = true;
+std::mutex        gGLFWContextMutex{};
 
 // Functions for GLFW
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
@@ -33,17 +41,17 @@ QString getAbsolutePathOfExecutableDirectory();
 QString getAbsolutePathRelativeToExecutable(const QString &path);
 
 // Shaders functions
-unsigned int initShaderProgram(int shaderCount, unsigned int *shaders,
-                               unsigned int *shaderTypes);
-unsigned int compileShader(unsigned int shader, const QString &source);
-void         shaderWatcher(const bool &running, GLFWwindow *window,
-                           unsigned int shaderProgram, int shaderCount,
-                           unsigned int *shaders, const QString *shaderFileNames);
+GLuint initShaderProgram(std::vector<unsigned int>       &shaders,
+                         const std::vector<unsigned int> &shaderTypes);
+void   compileShader(GLuint shader, const QString &source);
+void   shaderWatcher(GLFWwindow *window, GLuint shaderProgram,
+                     const std::vector<unsigned int> &shaders,
+                     const std::vector<QString>      &shaderFileNames);
 
 // Mesh functions
-unsigned int initMesh(const float vertices[], int vertexCount,
-                      const unsigned int indices[], int triangleCount);
-void drawMesh(unsigned int vao, int triangleCount, unsigned int shaderProgram);
+GLuint initMesh(const std::vector<float>        &vertices,
+                const std::vector<unsigned int> &indices);
+void   drawMesh(GLuint vao, GLsizei vertexCount, GLuint shaderProgram);
 
 // Main function
 int main(int argc, char *argv[]) {
@@ -77,7 +85,7 @@ int main(int argc, char *argv[]) {
 
     return -1;
   }
-  // Making current context attached to created window
+  // Capturing OpenGL context
   glfwMakeContextCurrent(window);
 
   // Loading OpenGL function using GLAD
@@ -97,46 +105,54 @@ int main(int argc, char *argv[]) {
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
   // Creating arrays of shaders, according filenames and types
-  constexpr unsigned int kShaderCount    = 2;
-  unsigned int          *shaders         = new unsigned int[kShaderCount]{};
-  QString               *shaderFileNames = new QString[kShaderCount]{
+  std::vector<unsigned int> shaders{0, 0};
+  std::vector<QString>      shaderFileNames{
       getAbsolutePathRelativeToExecutable("vertexShader.glsl"),
       getAbsolutePathRelativeToExecutable("fragmentShader.glsl"),
   };
-  unsigned int *shaderTypes = new unsigned int[kShaderCount]{
+  std::vector<unsigned int> shaderTypes{
       GL_VERTEX_SHADER,
       GL_FRAGMENT_SHADER,
   };
   // Creating and configuring a shader program
-  unsigned int shaderProgram =
-      initShaderProgram(kShaderCount, shaders, shaderTypes);
+  GLuint shaderProgram =
+      initShaderProgram(std::ref(shaders), std::cref(shaderTypes));
   // Running shaderWatcher thread
-  bool        shaderWatcherThreadRunning = true;
-  std::thread shaderWatcherThread{
-      shaderWatcher,  std::ref(shaderWatcherThreadRunning),
-      window,         shaderProgram,
-      kShaderCount,   shaders,
-      shaderFileNames};
+  gShaderWatcherIsRunning = true;
+  std::thread shaderWatcherThread{shaderWatcher, window, shaderProgram,
+                                  std::cref(shaders),
+                                  std::cref(shaderFileNames)};
 
   // Vertices and their indices to make a mesh from triangles
-  constexpr float kVertices[] = {
+  std::vector<float> vertices{
       -0.5f,  0.5f,  0.0f,
       0.5f,  0.5f,  0.0f,
       -0.25f, -0.5f, 0.0f,
       0.25f, -0.5f, 0.0f,
   };
-  constexpr unsigned int kIndices[] = {
+  std::vector<unsigned int> indices{
       0, 1, 2,
       1, 3, 2,
   };
-
   // Creating and configuring a mesh and getting its VAO
-  unsigned int vao =
-      initMesh(kVertices, sizeof(kVertices) / 3 / sizeof(float), kIndices,
-               sizeof(kIndices) / 3 / sizeof(unsigned int));
+  GLuint vao = initMesh(std::cref(vertices), std::cref(indices));
 
-  // While the window is open
-  while (!glfwWindowShouldClose(window)) {
+  // Releasing OpenGL context
+  glfwMakeContextCurrent(nullptr);
+
+  // Creating gGLFWContextMutex unique_lock
+  std::unique_lock<std::mutex> gGlfwContextLock{gGLFWContextMutex,
+                                                std::defer_lock};
+
+  // Render cycle
+  while (true) {
+    // Capturing mutex and OpenGL context
+    gGlfwContextLock.lock();
+    glfwMakeContextCurrent(window);
+
+    // If window should close
+    if (glfwWindowShouldClose(window)) break;
+
     // Processing window events
     glfwPollEvents();
 
@@ -147,19 +163,21 @@ int main(int argc, char *argv[]) {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Drawing the mesh
-    drawMesh(vao, sizeof(kIndices) / 3 / sizeof(unsigned int), shaderProgram);
+    // Drawing mesh
+    drawMesh(vao, vertices.size(), shaderProgram);
 
-    // Swapping the front and back buffers
+    // Swapping front and back buffers
     glfwSwapBuffers(window);
+
+    // Releasing OpenGL context and mutex
+    glfwMakeContextCurrent(nullptr);
+    gGlfwContextLock.unlock();
+
+    std::this_thread::sleep_for(kRenderCycleInterval);
   }
 
-  delete[] shaders;
-  delete[] shaderFileNames;
-  delete[] shaderTypes;
-
   // Waiting for shaderWatcher to stop
-  shaderWatcherThreadRunning = false;
+  gShaderWatcherIsRunning = false;
   shaderWatcherThread.join();
   // Terminating GLFW
   glfwTerminate();
@@ -170,7 +188,7 @@ int main(int argc, char *argv[]) {
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
-  // Setting viewport position and size relative to the window
+  // Setting viewport position and size relative to window
   glViewport(0, 0, width, height);
 }
 
@@ -198,13 +216,18 @@ QString getAbsolutePathRelativeToExecutable(const QString &path) {
   return getAbsolutePathOfExecutableDirectory() + QDir::separator() + path;
 }
 
-unsigned int initShaderProgram(int shaderCount, unsigned int *shaders,
-                               unsigned int *shaderTypes) {
+GLuint initShaderProgram(std::vector<unsigned int>       &shaders,
+                         const std::vector<unsigned int> &shaderTypes) {
+  size_t shaderCount = shaders.size();
+
+  // If sizes mismatch
+  if (shaderCount != shaderTypes.size()) return 0;
+
   // Creating a shader program
-  unsigned int shaderProgram = glCreateProgram();
+  GLuint shaderProgram = glCreateProgram();
 
   // For each shader
-  for (int i = 0; i < shaderCount; ++i) {
+  for (size_t i = 0; i < shaderCount; ++i) {
     // Creating a shader
     shaders[i] = glCreateShader(shaderTypes[i]);
     // Attaching shader to shader program
@@ -214,27 +237,28 @@ unsigned int initShaderProgram(int shaderCount, unsigned int *shaders,
   return shaderProgram;
 }
 
-unsigned int compileShader(unsigned int shader, const QString &source) {
+void compileShader(GLuint shader, const QString &source) {
   // Setting shader's source and compiling it
   const char *shaderSource = source.toLocal8Bit().data();
   glShaderSource(shader, 1, &shaderSource, nullptr);
   glCompileShader(shader);
 
   // Getting compilation success code
-  int success{};
+  GLint success{};
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   // Getting info log length
-  int infoLogLength{};
+  GLint infoLogLength{};
   glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
   // Getting info log
-  char *infoLog = new char[infoLogLength]{};
-  glGetShaderInfoLog(shader, infoLogLength, nullptr, infoLog);
+  std::vector<char> infoLog{};
+  infoLog.resize(infoLogLength + 1);
+  glGetShaderInfoLog(shader, infoLogLength, nullptr, &infoLog[0]);
   // If compilation have failed
   if (success == GL_FALSE) {
     std::cout << "error: unable to compile shader #" << shader << "\n";
     // If there is an info log
     if (infoLogLength > 0) {
-      std::cout << infoLog;
+      std::cout << &infoLog[0];
     }
     // If there is no info log
     else {
@@ -244,29 +268,37 @@ unsigned int compileShader(unsigned int shader, const QString &source) {
   }
   // If compilation have succeed and there is an info log
   else if (infoLogLength > 0) {
-    std::cout << "info: shader #" << shader << "\n" << infoLog << std::endl;
+    std::cout << "info: shader #" << shader << "\n" << &infoLog[0] << std::endl;
   }
-  delete[] infoLog;
-
-  return shader;
 }
 
-void shaderWatcher(const bool &running, GLFWwindow *window,
-                   unsigned int shaderProgram, int shaderCount,
-                   unsigned int *shaders, const QString *shaderFileNames) {
-  // Making current context attached to created window
-  glfwMakeContextCurrent(window);
+void shaderWatcher(GLFWwindow *window, GLuint shaderProgram,
+                   const std::vector<unsigned int> &shaders,
+                   const std::vector<QString>      &shaderFileNames) {
+  size_t shaderCount = shaders.size();
 
-  // Creating an array of last modification times
-  qint64 *shaderLastModificationTimes = new qint64[shaderCount]{};
+  // If sizes mismatch
+  if (shaderCount != shaderFileNames.size()) return;
+
+  // Creating and initializaing an array of last modification times
+  std::vector<qint64> shaderLastModificationTimes{};
+  shaderLastModificationTimes.assign(shaderCount, 0);
+
+  // Creating gGLFWContextMutex unique_lock
+  std::unique_lock<std::mutex> gGlfwContextLock{gGLFWContextMutex,
+                                                std::defer_lock};
 
   // While thread is running
-  while (running) {
+  while (gShaderWatcherIsRunning) {
+    // Capturing mutex and OpenGL context
+    gGlfwContextLock.lock();
+    glfwMakeContextCurrent(window);
+
     // No shaders are modified yet
     bool modified = false;
 
     // For each shader
-    for (int i = 0; i < shaderCount; ++i) {
+    for (size_t i = 0; i < shaderCount; ++i) {
       QFile shaderFile{shaderFileNames[i]};
       if (!shaderFile.open(QFile::ReadOnly | QFile::Text)) {
         std::cout << "error: unable to find "
@@ -277,18 +309,19 @@ void shaderWatcher(const bool &running, GLFWwindow *window,
       qint64    shaderModificationTime =
           shaderFileInfo.lastModified().toMSecsSinceEpoch();
 
+      // If shader was modified
       if (shaderModificationTime > shaderLastModificationTimes[i]) {
-        // Reading shader source code from the file
+        // Reading shader source code from file
         QTextStream shaderFileTextStream{&shaderFile};
         QString     shaderSource{shaderFileTextStream.readAll()};
 
-        // Compiling the shader
+        // Compiling shader
         compileShader(shaders[i], shaderSource);
 
         // Updating last modification time
         shaderLastModificationTimes[i] = shaderModificationTime;
 
-        // The shader is modified
+        // Shader is modified
         modified = true;
       }
     }
@@ -299,21 +332,22 @@ void shaderWatcher(const bool &running, GLFWwindow *window,
       glLinkProgram(shaderProgram);
 
       // Getting linkage success code
-      int success{};
+      GLint success{};
       glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
       // Getting linkage info log length
-      int infoLogLength{};
+      GLint infoLogLength{};
       glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
       // Getting linkage info log
-      char *infoLog = new char[infoLogLength]{};
-      glGetProgramInfoLog(shaderProgram, infoLogLength, nullptr, infoLog);
+      std::vector<char> infoLog{};
+      infoLog.resize(infoLogLength + 1);
+      glGetProgramInfoLog(shaderProgram, infoLogLength, nullptr, &infoLog[0]);
       // If linkage have failed
       if (success == GL_FALSE) {
         std::cout << "error: unable to link shaders in program #"
                   << shaderProgram << "\n";
         // If there is an info log
         if (infoLogLength > 0) {
-          std::cout << infoLog;
+          std::cout << &infoLog[0];
         }
         // If there is no info log
         else {
@@ -323,26 +357,27 @@ void shaderWatcher(const bool &running, GLFWwindow *window,
         // If linkage have succeed and there is an info log
       } else if (infoLogLength > 0) {
         std::cout << "info: shader program #" << shaderProgram << "\n"
-                  << infoLog << std::endl;
+                  << &infoLog[0] << std::endl;
       }
-      delete[] infoLog;
     }
+
+    // Releasing OpenGL context and mutex
+    glfwMakeContextCurrent(nullptr);
+    gGlfwContextLock.unlock();
 
     // Wait some time to approximately follow the time interval
     std::this_thread::sleep_for(kShaderWatcherInterval);
   }
-
-  delete[] shaderLastModificationTimes;
 }
 
-unsigned int initMesh(const float vertices[], int vertexCount,
-                      const unsigned int indices[], int triangleCount) {
+GLuint initMesh(const std::vector<float>        &vertices,
+                const std::vector<unsigned int> &indices) {
   // Creating VAO, VBO and EBO
-  unsigned int vao{};
+  GLuint vao{};
   glGenVertexArrays(1, &vao);
-  unsigned int vbo{};
+  GLuint vbo{};
   glGenBuffers(1, &vbo);
-  unsigned int ebo{};
+  GLuint ebo{};
   glGenBuffers(1, &ebo);
 
   // Binding VAO to bind to it VBO and EBO and then configure them
@@ -350,18 +385,16 @@ unsigned int initMesh(const float vertices[], int vertexCount,
 
   // Binding and filling VBO
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, vertexCount * 3 * sizeof(float), vertices,
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0],
                GL_STATIC_DRAW);
 
   // Binding and filling EBO
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-               triangleCount * 3 * sizeof(unsigned int), indices,
-               GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+               &indices[0], GL_STATIC_DRAW);
 
   // Configuring and enabling VBO's 0th attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                        reinterpret_cast<void *>(0));
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
   glEnableVertexAttribArray(0);
 
   // Unbinding configured VAO to use it later
@@ -370,7 +403,7 @@ unsigned int initMesh(const float vertices[], int vertexCount,
   return vao;
 }
 
-void drawMesh(unsigned int vao, int triangleCount, unsigned int shaderProgram) {
+void drawMesh(GLuint vao, GLsizei vertexCount, GLuint shaderProgram) {
   // Setting specific shader program to use for render
   glUseProgram(shaderProgram);
   // Binding VAO with associated VBO and EBO
@@ -378,7 +411,7 @@ void drawMesh(unsigned int vao, int triangleCount, unsigned int shaderProgram) {
 
   // Drawing mesh
   //glDrawArrays(GL_TRIANGLES, 0, 3); // without vertices (EBO) method
-  glDrawElements(GL_TRIANGLES, triangleCount * 3, GL_UNSIGNED_INT,
+  glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT,
                  0);  // with vertices (EBO) method
 
   // Unbinding VAO
